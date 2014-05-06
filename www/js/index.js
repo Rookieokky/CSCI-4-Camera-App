0a20f19b25dc2ec76a1132daadd207453e3b81bc
Boyd Trolinger <boyd.trolinger@gmail.com>
2014-05-06 06:29:30 -0700

    Updated index.js

--- www/js/index.js
+++ www/js/index.js
@@ -4,7 +4,7 @@ function savePhoto(data) {
     $('#cameraPic').attr('src', "data:image/jpeg;base64," + data);
 }
 
-$('#camera-control').click(function () {
+$('button.camera-control').click(function () {
 	++picsTaken;
    	$('#pics-taken').html(picsTaken + " pictures have been taken.");
     if (navigator.camera) {
