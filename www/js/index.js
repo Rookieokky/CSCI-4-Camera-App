82bc46d2f2a21311b47fbeb5aecc8a03421d8de0
Boyd Trolinger <boyd.trolinger@gmail.com>
2014-05-07 17:30:56 -0700

    Added geolocation code to index.js and some markup for photo info in index.html

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,21 @@
 var picsTaken = 0;
+var entries = [];
+
+/* execute immediately on load */
+reloadEntries();
 
 function getPhoto(data) {
     $('#camera-photo').attr('src', "data:image/jpeg;base64," + data);
+}
+
+function getPosition (position) {
+    if (navigator.geolocation) {
+        var longitude = position.coords.latitude;
+        var latitude = position.coords.longitude;
+        $('#gis-location').html("GIS coordinates: " + longitude + ", " + latitude);
+    } else {
+        $('#gis-location').html("GIS coordinates: not available");
+    }
 }
 
 function photoError () {
@@ -19,8 +33,42 @@ $('button.camera-control').click(function () {
     		encodingType: 0
     	};
     	navigator.camera.getPicture(getPhoto, photoError, options);
+        navigator.geolocation.getCurrentPosition(getPosition, null);
     }
     else {
     	$('#error-output').html('CameraAPI not supported');
     }
 });
+
+$('button.save').click(function () {
+    entries.append(makeEntry());
+    saveEntries();
+});
+
+function makeEntry () {
+    var currentPhoto = $('#camera-photo').attr('src');
+    var currentGisLocation = $('#gis-location').html();
+    var currentDescription = $('#description').val();
+
+    var entry = {
+        "photo" : currentPhoto,
+        "gisLocation" : currentGisLocation, 
+        "description" : currentDescription
+    };
+    return entry;
+}
+
+function reloadEntries () {
+    if (Modernizr.localstorage) {
+        if (localStorage["entries"] != null) {
+            entries = JSON.parse(localStorage["entries"]);
+        }
+    }
+}
+
+function saveEntries () {
+    if (Modernizr.localstorage) {
+        localStorage.clear();
+            localStorage["entries"] = JSON.stringify(entries);
+    }
+}
